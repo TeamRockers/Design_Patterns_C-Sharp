60bc32cb109647b4c1e620d995776df6f1c5c0e1
Dinesh Kumar <ashokdinesh89@gmail.com>
2020-09-27 20:57:10 +0530

    Abstract Factory and Builder Pattern

---
+++ Abstract Factory/Abstract Factory/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Abstract_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContinentFactory continentFactory = new AmericaContinent();
+            AnimalFactory animalFactory = new AnimalFactory(continentFactory);
+            animalFactory.wildFoodChain();
+
+            continentFactory = new AfricaContinent();
+            animalFactory = new AnimalFactory(continentFactory);
+            animalFactory.wildFoodChain();
+
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class ContinentFactory
+    {
+        public abstract Herbivore createHerbivore();
+        public abstract Carnivore createCarnivore();
+    }
+
+    public  class AfricaContinent: ContinentFactory
+    {
+        public override Carnivore createCarnivore()
+        {
+            return new Lion();
+        }
+
+        public  override Herbivore createHerbivore()
+        {
+            return new Goat();
+        }
+
+    }
+
+    public  class AmericaContinent : ContinentFactory
+    {
+        public override Carnivore createCarnivore()
+        {
+            return new Wolf();
+        }
+
+        public override Herbivore createHerbivore()
+        {
+            return new Deer();
+        }
+    }
+
+    public abstract class Herbivore
+    {
+
+    }
+
+    public abstract class Carnivore
+    {
+        public abstract void Eat(Herbivore h);
+    }
+
+    public  class Lion : Carnivore
+    {
+        public override void Eat(Herbivore h)
+        {
+            Console.WriteLine(this.GetType().Name + " eats " + h.GetType().Name);
+        }
+    }
+
+    public class Goat : Herbivore
+    {
+
+    }
+
+    public class Wolf: Carnivore
+    {
+        public override void Eat(Herbivore h)
+        {
+            Console.WriteLine(this.GetType().Name + " eats " + h.GetType().Name);
+        }
+    }
+
+    public class Deer : Herbivore
+    {
+
+    }
+
+
+    public class AnimalFactory
+    {
+        private Herbivore _herbivore;
+        private Carnivore _carnivore;
+
+        public AnimalFactory(ContinentFactory continentFactory)
+        {
+          _herbivore =   continentFactory.createHerbivore();
+          _carnivore = continentFactory.createCarnivore();
+
+        }
+
+        public void wildFoodChain()
+        {
+            _carnivore.Eat(_herbivore);
+        }
+    }
+}
---
+++ BuilderPattern/BuilderPattern/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuilderPattern
+{
+ 
+
+    public abstract class Builder
+    {
+        public abstract void BuildPartA();
+        public abstract void BuildPartB();
+        public abstract void BuildPartC();
+        public abstract Product GetProduct();
+    }
+
+    public class Concrete1 : Builder
+    {
+        public Product product;
+
+        public Concrete1()
+        {
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            this.product = new Product();
+        }
+
+        public override void BuildPartA()
+        {
+            this.product.add("PART 1");
+        }
+
+        public override void BuildPartB()
+        {
+            this.product.add("PART 2");
+        }
+
+        public override void BuildPartC()
+        {
+            this.product.add("PART 3");
+        }
+
+        public override Product GetProduct()
+        {
+            Product product1 = this.product;
+            this.Reset();
+            return product1;
+        }
+    }
+
+    public class Director
+    {
+        protected Builder _builder;
+
+        public Director(Builder builder)
+        {
+            this._builder = builder;
+        }
+
+        public void BuildMin()
+        {
+            this._builder.BuildPartA();
+        }
+
+        public void BuildMax()
+        {
+            this._builder.BuildPartA();
+            this._builder.BuildPartB();
+            this._builder.BuildPartC();
+        }
+        
+    }
+
+    public class Product
+    {
+        private List<string> parts = new List<string>();
+
+        public void add(string partName)
+        {
+            this.parts.Add(partName);
+        }
+
+        public void ListObjects()
+        {
+            for(int i = 0; i<parts.Count;i++)
+            {
+                Console.WriteLine(parts[i]);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Builder builder = new Concrete1();
+            Director director = new Director(builder);
+            director.BuildMax();
+            builder.GetProduct().ListObjects();
+
+            director.BuildMin();
+            builder.GetProduct().ListObjects();
+
+            builder.BuildPartC();
+            builder.GetProduct().ListObjects();
+
+        }
+    }
+}

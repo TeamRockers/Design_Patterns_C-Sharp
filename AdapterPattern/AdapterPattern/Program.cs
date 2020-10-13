f47505d95629fe6b35c80884f6a0c9b9b5dfaaae
Dinesh Kumar <ashokdinesh89@gmail.com>
2020-10-13 22:23:12 +0530

    Adapter Pattern

---
+++ AdapterPattern/AdapterPattern/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Target tr = new Adapter();
+            tr.Display();
+            Console.ReadLine();
+        }
+    }
+
+    class Target
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("Target Request");
+        }
+    }
+
+    class Adapter: Target
+    {
+        private Adaptee adaptee = new Adaptee();
+
+        public override void Display()
+        {
+            adaptee.SpecificRequest();
+        }
+    }
+
+    class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Adapter Request");
+        }
+    }
+}
